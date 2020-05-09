fd8d36f96968444f1bf238b4d69f157bc2d8d4d9
MireilySilva <64507099+MireilySilva@users.noreply.github.com>
2020-05-09 00:39:34 -0500

    Segunda Modificación de código Java desde GitHub
    Aquí estamos modificando el código Java desde el editor de texto de la misma cuenta GitHub.

--- Proyectobasico01/src/main/java/Repositorio/Programa01.java
+++ Proyectobasico01/src/main/java/Repositorio/Programa01.java
@@ -7,6 +7,7 @@ public class Programa01 {
    
     public static void main(String[] args) {
         System.out.println("HOLA SOY MIREILY SILVA ROMERO");
+        System.out.println("Estoy agregando este código desde la misma cuenta de GitHub desde este momento!!!");
     }
     
 }
